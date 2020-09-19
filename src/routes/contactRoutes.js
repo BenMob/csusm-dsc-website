e62b0b1a29e813aaaab6f492c4b15b04533d71b3
Benjamin <omben001@cougars.csusm.edu>
2020-09-19 15:24:22 -0700

    Adds contact routes

---
+++ src/routes/contactRoutes.js
@@ -0,0 +1,12 @@
+/**
+ * Contact Routes
+ */
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('contact')
+})
+
+module.exports = router
